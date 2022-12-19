ab75503e5ebd8bce4d82863eb5daee16f5407563
email <shekharjoshiofficial@gmail.com>
2022-12-19 15:19:11 +0530

    Updates

---
+++ precache-manifest.df1ac074f3ecd3bd751e2f6e52b60659.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40f64ca350fd05af9dfd112bace7c1fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97815e4569b7dd649384",
+    "url": "/static/css/2.451bf2e4.chunk.css"
+  },
+  {
+    "revision": "5ef43295188d56775ac5",
+    "url": "/static/css/main.6f07e2c0.chunk.css"
+  },
+  {
+    "revision": "97815e4569b7dd649384",
+    "url": "/static/js/2.6d107c3b.chunk.js"
+  },
+  {
+    "revision": "b5df7406764e81555a626ff33320ad29",
+    "url": "/static/js/2.6d107c3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ef43295188d56775ac5",
+    "url": "/static/js/main.8e1e009b.chunk.js"
+  },
+  {
+    "revision": "efe25b4b8d1f7af9da4b",
+    "url": "/static/js/runtime-main.6b3488d1.js"
+  },
+  {
+    "revision": "20e45d9afe66e0e6533dcd44fb9a7846",
+    "url": "/static/media/dev.20e45d9a.svg"
+  },
+  {
+    "revision": "e324d3000236bc43b95fd0d1d88ec37f",
+    "url": "/static/media/shake.e324d300.svg"
+  }
+]);
